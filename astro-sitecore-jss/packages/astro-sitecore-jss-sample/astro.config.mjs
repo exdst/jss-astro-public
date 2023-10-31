9db92a426b524eeddc9ae3bb0c27f026d1ededc3
Bogdan Druziuk <bdruziuk@gmail.com>
2023-11-01 01:49:36 +0200

    add samples for vue JS integration

--- astro-sitecore-jss/packages/astro-sitecore-jss-sample/astro.config.mjs
+++ astro-sitecore-jss/packages/astro-sitecore-jss-sample/astro.config.mjs
@@ -1,12 +1,13 @@
 import { defineConfig } from 'astro/config';
 import react from "@astrojs/react";
+import vue from "@astrojs/vue";
 import image from "@astrojs/image";
 
 import node from "@astrojs/node";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [react(), image({
+  integrations: [react(), vue(),  image({
     serviceEntryPoint: '@astrojs/image/sharp'
   })],
   output: 'server',
---
+++ astro-sitecore-jss/packages/astro-sitecore-jss-sample/src/components/integrations/vue/Counter.vue
@@ -0,0 +1,26 @@
+<template>
+    <div className="flex">
+      <div>
+        Count: {{ count }}
+      </div>
+      <button v-on:click="count += 1" class="btn btn-primary m-2">
+        Increment
+      </button>
+      <button v-on:click="count -= 1" class="btn btn-primary m-2">
+        Decrement
+      </button>
+    </div>
+</template>
+
+<script>
+
+export default {
+  name: 'Counter',
+  props: {
+    count: {
+      type: Number,
+      default: 0,
+    }
+  }
+};
+</script>
