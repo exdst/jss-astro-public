5496de992446fb2f44180b4dfb93c1652dca108d
Antonytm <antontishchenko@gmail.com>
2023-11-01 22:47:52 +0200

    Update sample for nested REACT placeholders

--- astro-sitecore-jss/packages/astro-sitecore-jss-sample/src/components/integrations/react/ContentBlock.jsx
+++ astro-sitecore-jss/packages/astro-sitecore-jss-sample/src/components/integrations/react/ContentBlock.jsx
@@ -1,7 +1,5 @@
 import React from 'react';
 import { Text, RichText } from '@sitecore-jss/sitecore-jss-react';
-//import { Placeholder } from '@sitecore-jss/sitecore-jss-react';
-//import ComponentFactory from "./ComponentFactory";
 
 /**
  * A simple Content Block component, with a heading and rich text block.
@@ -15,12 +13,33 @@ const ContentBlock = (props) => {
     <div className="contentBlock">
       <Text tag="h6" className="contentTitle" field={fields.heading} />
       <RichText className="contentDescription" field={fields.description} />
-      {/*
-      Example of using a placeholder inside React compoents
-      <Placeholder name="react" rendering={props.rendering} componentFactory={ComponentFactory} />
-      */}
     </div>
   );
 }
 
 export default ContentBlock;
+
+
+/**
+ *  Demo for Nested React placholders
+ *  How component should look like if you want to use REACT Placeholder inside it
+import React from 'react';
+import { Text, RichText } from '@sitecore-jss/sitecore-jss-react';
+import { Placeholder } from '@sitecore-jss/sitecore-jss-react';
+import ComponentFactory from "./ComponentFactory";
+
+
+const ContentBlock = (props) => {
+  const fields = props.fields;
+
+  return (
+    <div className="contentBlock">
+      <Text tag="h6" className="contentTitle" field={fields.heading} />
+      <RichText className="contentDescription" field={fields.description} />
+      <Placeholder name="react" rendering={props.rendering} componentFactory={ComponentFactory} />
+    </div>
+  );
+}
+
+export default ContentBlock;
+ */
