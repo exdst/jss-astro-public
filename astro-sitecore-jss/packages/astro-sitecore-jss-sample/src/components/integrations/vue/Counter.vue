2673cf0cd218dcf1bb93d8056a9724114d85fdca
Bogdan Druziuk <bdruziuk@gmail.com>
2023-11-02 15:16:35 +0200

    fix vue integration placeholder

--- astro-sitecore-jss/packages/astro-sitecore-jss-sample/src/components/integrations/vue/Counter.vue
+++ astro-sitecore-jss/packages/astro-sitecore-jss-sample/src/components/integrations/vue/Counter.vue
@@ -13,7 +13,6 @@
 </template>
 
 <script>
-
 export default {
   name: 'Counter',
   props: {
---
+++ astro-sitecore-jss/packages/astro-sitecore-jss-sample/src/components/integrations/vue/Placeholder.vue
@@ -0,0 +1,29 @@
+<template>
+  <sc-placeholder 
+    name="vueph" 
+    :rendering="rendering"
+    :params="params"
+    :componentFactory="componentFactory" />  
+</template>
+
+<script>
+import { Placeholder } from '@sitecore-jss/sitecore-jss-vue';
+
+export default {
+  name: 'Placeholder',
+  props: {
+    rendering: {
+      type: Object,
+    },
+    params: {
+      type: Object,
+    },
+    componentFactory: {
+      type: Object
+    }
+  },
+  components: {
+    ScPlaceholder: Placeholder
+  },
+};
+</script>
