707a6563489335475b60b7cf4b28b070e0fb01c1
Bogdan Druziuk <bdruziuk@gmail.com>
2023-11-06 23:01:03 +0200

    update astro dependencies and other npm packages

--- astro-sitecore-jss/packages/astro-sitecore-jss-sample/astro.config.mjs
+++ astro-sitecore-jss/packages/astro-sitecore-jss-sample/astro.config.mjs
@@ -1,15 +1,12 @@
 import { defineConfig } from 'astro/config';
 import react from "@astrojs/react";
 import vue from "@astrojs/vue";
-import image from "@astrojs/image";
 
 import node from "@astrojs/node";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [react(), vue(),  image({
-    serviceEntryPoint: '@astrojs/image/sharp'
-  })],
+  integrations: [react(), vue()],
   output: 'server',
   adapter: node({
     mode: 'standalone'
