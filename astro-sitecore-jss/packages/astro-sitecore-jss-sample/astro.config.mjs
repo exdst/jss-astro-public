15a732f25aac26d0f179f15956e7669e63229446
Antonytm <antontishchenko@gmail.com>
2023-11-17 03:03:03 +0200

    Attempt to fix build issue

--- astro-sitecore-jss/packages/astro-sitecore-jss-sample/astro.config.mjs
+++ astro-sitecore-jss/packages/astro-sitecore-jss-sample/astro.config.mjs
@@ -3,17 +3,9 @@ import react from '@astrojs/react';
 import vue from '@astrojs/vue';
 import node from '@astrojs/node';
 import angular from '@analogjs/astro-angular';
+import path from 'path';
+import tsconfigPaths from 'vite-tsconfig-paths'
 
-// https://astro.build/config
-/*
-  Vite config is required to resolve the alias for the local package
-  Astro 2.+ was working based on tsconfig.json, but Astro 3.+ is not.
-  compilerOptions > paths is not taken into account.
-  There was opened Github ticket for this issue:
-  https://github.com/withastro/astro/issues/9015
-  The other option is to consider vite-tsconfig-paths plugin, but it is not working with Astro 3.+ as well. (After short try) 
-  https://www.npmjs.com/package/vite-tsconfig-paths
-*/
 export default defineConfig({
   integrations: [react(), vue(), angular()],
   output: 'server',
@@ -22,13 +14,6 @@ export default defineConfig({
   }),
   outDir: './dist',
   vite: {
-    resolve: {
-      alias: [
-        {
-          find: '@astro-sitecore-jss/astro-sitecore-jss',
-          replacement: 'packages/astro-sitecore-jss',
-        },
-      ],
-    },
+    plugins: [tsconfigPaths()]
   },
 });
