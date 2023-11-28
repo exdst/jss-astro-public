682fb12e5c9770b746e0b1dea9e34c17f61920a8
Antonytm <antontishchenko@gmail.com>
2023-11-28 16:42:36 +0200

    Fix project initialization: Add Angular configuration

--- astro-sitecore-jss/packages/astro-sitecore-jss-sample/astro.config.mjs
+++ astro-sitecore-jss/packages/astro-sitecore-jss-sample/astro.config.mjs
@@ -4,16 +4,6 @@ import vue from '@astrojs/vue';
 import node from '@astrojs/node';
 import angular from "@analogjs/astro-angular";
 
-/*
-  Vite config is required to resolve the alias for the local package
-  Astro 2.+ was working based on tsconfig.json, but Astro 3.+ is not.
-  compilerOptions > paths is not taken into account.
-  There was opened Github ticket for this issue:
-  https://github.com/withastro/astro/issues/9015
-  The other option is to consider vite-tsconfig-paths plugin, but it is not working with Astro 3.+ as well. (After short try) 
-  https://www.npmjs.com/package/vite-tsconfig-paths
-*/
-
 // https://astro.build/config
 export default defineConfig({
   integrations: [react(), vue(), angular({
