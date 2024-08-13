829e57509693b6a2ed8a71412b89a859cac47e42
Bogdan Druziuk <bdruziuk@gmail.com>
2024-08-13 16:35:28 +0300

    Fix issue with astro react integration after update dependecies

--- astro-sitecore-jss/packages/astro-sitecore-jss-sample/astro.config.mjs
+++ astro-sitecore-jss/packages/astro-sitecore-jss-sample/astro.config.mjs
@@ -4,15 +4,37 @@ import vue from '@astrojs/vue';
 import node from '@astrojs/node';
 import angular from "@analogjs/astro-angular";
 
+const reactConfig = {
+  babel: {
+    "presets": [
+      [
+        "@babel/preset-react",
+        {
+          "runtime": "classic"
+        }
+      ]
+    ],
+    "plugins": [
+      ["@babel/plugin-proposal-decorators", { "legacy": true }]
+    ]
+  }
+}
+
+const angularConfig = {
+  vite: {
+    transformFilter: (code, id ) => {
+      return !id.includes('/packages/astro-sitecore-jss/')
+    },
+  }
+}
+
 // https://astro.build/config
 export default defineConfig({
-  integrations: [react(), vue(), angular({
-    vite: {
-      transformFilter: (code, id ) => {
-        return !id.includes('/packages/astro-sitecore-jss/')
-      },
-    }
-  })],
+  integrations: [
+    react(reactConfig),
+    vue(),
+    angular(angularConfig)
+  ],
   output: 'server',
   adapter: node({
     mode: 'standalone',
