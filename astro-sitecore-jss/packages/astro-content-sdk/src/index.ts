ec9a5c4795dd186690bfaadb7bd9c0ca37d1b4ba
Vadym Shcherban <vs@exdst.com.ua>
2025-09-12 15:19:13 +0300

    Update package exports

--- astro-sitecore-jss/packages/astro-content-sdk/src/index.ts
+++ astro-sitecore-jss/packages/astro-content-sdk/src/index.ts
@@ -1,1 +1,16 @@
-export { AstroContentSdkComponent, ComponentMap } from './sharedTypes/component-props';
+export { HTMLLink } from '@sitecore-content-sdk/core';
+
+export {
+  Field,
+  LayoutServiceData,
+  ComponentRendering,
+  ComponentFields,
+  ComponentParams,
+} from '@sitecore-content-sdk/core/layout';
+
+export {
+  AstroContentSdkComponent,
+  ComponentMap,
+} from './sharedTypes/component-props';
+
+export { PageMode, ErrorPage, Page } from '@sitecore-content-sdk/core/client';
